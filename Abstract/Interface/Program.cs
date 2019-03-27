09936023111b29890cc73faa4e30eae34c200046
Balaji3410 <48978007+Balaji3410@users.noreply.github.com>
2019-03-27 14:32:52 +0530

    Add files via upload

---
+++ Abstract/Interface/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+public interface Drawshape
+{
+    void draw();
+}
+public class Rectangle : Drawshape
+{
+    public void draw()
+    {
+        Console.WriteLine("drawing rectangle...");
+    }
+}
+public class Circle : Drawshape
+{
+    public void draw()
+    {
+        Console.WriteLine("drawing circle...");
+    }
+}
+public class TestInterface
+{
+    public static void Main()
+    {
+        Drawshape d;
+        d = new Rectangle();
+        d.draw();
+        d = new Circle();
+        d.draw();
+        Console.ReadLine();
+    }
+}
